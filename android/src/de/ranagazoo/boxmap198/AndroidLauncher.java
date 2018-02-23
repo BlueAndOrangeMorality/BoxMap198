d686a629966643a5aeabf5a3b4e5dac02ba6f5ef
Kai Köpke <kai.koepke@quinscape.de>
2018-02-23 13:27:23 +0100

    BoxMap198: Initial commit

---
+++ android/src/de/ranagazoo/boxmap198/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.ranagazoo.boxmap198;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.ranagazoo.boxmap198.BoxMap198;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BoxMap198(), config);
+	}
+}
---
+++ desktop/src/de/ranagazoo/boxmap198/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.ranagazoo.boxmap198.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.ranagazoo.boxmap198.BoxMap198;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BoxMap198(), config);
+	}
+}
